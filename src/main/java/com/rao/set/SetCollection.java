bcb407ea66ade7aeaab9b66ad5dc0a0ac62e2b70
unknown <1145333729@qq.com>
2021-06-06 21:27:03 +0800

    '删掉不要的'

--- src/main/java/com/rao/set/SetCollection.java
+++ src/main/java/com/rao/set/SetCollection.java
@@ -1,4 +1,7 @@
 package com.rao.set;
+
+import java.util.HashSet;
+
 /*
 * 博客地址https://blog.csdn.net/a724888/article/details/80295328
 * set的基础和原理
@@ -8,7 +11,15 @@ package com.rao.set;
 *
 * 3、LinkedHashSet
 *   是hashSet的扩展版本
+*   LinkedHashSet 通过继承 HashSet，底层使用 LinkedHashMap
 * */
 public class SetCollection {
 
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(12);
+        System.out.println(set.toString());
+        set.remove(12);
+        System.out.println(set.toString());
+    }
 }
